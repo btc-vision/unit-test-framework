ede190ca6939647f363a75588eba3c434d71de1c
matbout <48333440+matbout@users.noreply.github.com>
2025-04-04 15:48:56 +0400

    Limit memory per tx

---
+++ test/e2e/memory/Memory.ts
@@ -0,0 +1,55 @@
+import { Address } from '@btc-vision/transaction';
+import { Assert, Blockchain, opnet, OPNetUnit } from '../../../src';
+import { TestContractRuntime } from '../test-contract/runtime/TestContractRuntime';
+
+await opnet('Memory tests', async (vm: OPNetUnit) => {
+    let contract: TestContractRuntime;
+
+    const deployerAddress: Address = Blockchain.generateRandomAddress();
+    const contractAddress: Address = Blockchain.generateRandomAddress();
+
+    vm.beforeEach(async () => {
+        Blockchain.dispose();
+        Blockchain.clearContracts();
+        await Blockchain.init();
+
+        contract = new TestContractRuntime(deployerAddress, contractAddress);
+        Blockchain.register(contract);
+        await contract.init();
+
+        Blockchain.txOrigin = deployerAddress;
+        Blockchain.msgSender = deployerAddress;
+    });
+
+    vm.afterEach(() => {
+        contract.dispose();
+        Blockchain.dispose();
+    });
+
+    await vm.it('should grow the memory to the maximum size', async () => {
+        let result = await contract.growMemory(511);
+        Assert.expect(result).toEqual(true);
+    });
+
+    await vm.it('should fail to grow the memory over the maximum size', async () => {
+        let result = await contract.growMemory(512);
+        Assert.expect(result).toEqual(false);
+    });
+
+    await vm.it('should grow the memory to the maximum size after a call', async () => {
+        let result = await contract.callThenGrowMemory(510);
+        Assert.expect(result).toEqual(true);
+    });
+
+    await vm.it('should fail to grow the memory over the maximum size after a call', async () => {
+        let result = await contract.callThenGrowMemory(511);
+        Assert.expect(result).toEqual(false);
+    });
+
+    // TODO: Hangs after completion
+    // await vm.it('should fail to make a call after growing the memory to the maximum size', async () => {
+    //     await Assert.expect(async () => {
+    //         await contract.growMemoryThenRecursiveCall(511, 1);
+    //     }).toThrow("No more memory pages available")
+    // });
+});
--- test/e2e/test-contract/contract/TestContract.ts
+++ test/e2e/test-contract/contract/TestContract.ts
@@ -1,4 +1,10 @@
-import { BytesWriter, Calldata, OP_NET } from '@btc-vision/btc-runtime/runtime';
+import {
+    Blockchain,
+    BytesWriter,
+    Calldata,
+    encodeSelector,
+    OP_NET,
+} from '@btc-vision/btc-runtime/runtime';
 import { sha256 } from '@btc-vision/btc-runtime/runtime/env/global';
 
 @final
@@ -7,21 +13,83 @@ export class TestContract extends OP_NET {
         super();
     }
 
-    @method({
-        name: 'value',
-        type: ABIDataTypes.BYTES,
-    })
-    @returns({
-        name: 'value',
-        type: ABIDataTypes.BYTES32,
-    })
+    @method('bytes')
+    @returns('bytes32')
     public sha256(calldata: Calldata): BytesWriter {
         const data = calldata.readBytesWithLength();
 
-        const result = sha256(data)
+        const result = sha256(data);
 
         const writer = new BytesWriter(32);
         writer.writeBytes(result);
         return writer;
+    }
+
+    @method()
+    @returns('bool')
+    public callThenGrowMemory(calldata: Calldata): BytesWriter {
+        const pages = calldata.readU32();
+
+        const growMemoryCalldata = new BytesWriter(8);
+        growMemoryCalldata.writeSelector(encodeSelector('growMemory(uint32)'));
+        growMemoryCalldata.writeU32(pages);
+
+        const response = Blockchain.call(this.address, growMemoryCalldata);
+        const success = response.readBoolean();
+
+        const result = new BytesWriter(1);
+        result.writeBoolean(success);
+
+        return result;
+    }
+
+    @method('uint32', 'uint32')
+    public growMemoryThenRecursiveCall(calldata: Calldata): BytesWriter {
+        const pages = calldata.readU32();
+        const numberOfCalls = calldata.readU32();
+
+        this._growMemory(pages);
+
+        const recursiveCallCalldata = new BytesWriter(8);
+        recursiveCallCalldata.writeSelector(encodeSelector('recursiveCall(uint32)'));
+        recursiveCallCalldata.writeU32(numberOfCalls - 1);
+        Blockchain.call(this.address, recursiveCallCalldata);
+
+        return new BytesWriter(0);
+    }
+
+    @method('uint32')
+    @returns('bool')
+    public growMemory(calldata: Calldata): BytesWriter {
+        const pages = calldata.readU32();
+
+        const success = this._growMemory(pages);
+
+        const result = new BytesWriter(1);
+        result.writeBoolean(success);
+        return result;
+    }
+
+    public _growMemory(pages: u32): boolean {
+        const previousMemorySize = memory.grow(pages);
+
+        return previousMemorySize != -1;
+    }
+
+    @method('uint32')
+    public recursiveCall(calldata: Calldata): BytesWriter {
+        const numberOfCalls = calldata.readU32();
+
+        if (numberOfCalls == 0) {
+            return new BytesWriter(0);
+        }
+
+        const recursiveCallCalldata = new BytesWriter(8);
+        recursiveCallCalldata.writeSelector(encodeSelector('recursiveCall(uint32)'));
+        recursiveCallCalldata.writeU32(numberOfCalls - 1);
+
+        Blockchain.call(this.address, recursiveCallCalldata);
+
+        return new BytesWriter(0);
     }
 }
