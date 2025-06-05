da91bb925a9f4dc0b6fe6be2087b03d1e3de6084
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2025-06-04 23:59:37 -0400

    Fixed compile

--- scripts/buildTestContracts.js
+++ scripts/buildTestContracts.js
@@ -2,7 +2,9 @@ import { execSync } from 'node:child_process';
 
 const root = process.cwd();
 process.chdir(`${root}/test/e2e/contracts/test-contract/contract`);
-execSync('asc index.ts --target debug --measure --uncheckedBehavior never');
+execSync('asc index.ts --config asconfig.json --target debug --measure --uncheckedBehavior never');
 
 process.chdir(`${root}/test/e2e/contracts/gas-test-contract/contract`);
-execSync('asc GasTestContract.ts --target debug --measure --uncheckedBehavior never');
+execSync(
+    'asc GasTestContract.ts --config asconfig.json --target debug --measure --uncheckedBehavior never',
+);
